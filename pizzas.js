23006a2852269183f0e49dcda24cfcabc5be8fb0
João Rafael <jrojoao@gmail.com>
2025-03-06 22:10:13 -0300

    ajustes

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,58 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Moda do Chef', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {
+        id: 1,
+        name: 'Mussarela',
+        img: 'images/pizza.png',
+        price: 25.90,
+        sizes: ['100g', '530g', '860g'],
+        description: 'A clássica pizza de mussarela, feita com molho de tomate especial, queijo derretido e orégano.'
+    },
+    {
+        id: 2,
+        name: 'Calabresa',
+        img: 'images/pizza2.png',
+        price: 23.50,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Deliciosa pizza de calabresa fatiada, cebola caramelizada e um toque de orégano.'
+    },
+    {
+        id: 3,
+        name: 'Quatro Queijos',
+        img: 'images/pizza3.png',
+        price: 27.90,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Uma combinação irresistível de mussarela, provolone, gorgonzola e parmesão.'
+    },
+    {
+        id: 4,
+        name: 'Americana',
+        img: 'images/pizza4.png',
+        price: 24.99,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Molho especial, mussarela, bacon crocante, milho verde e catupiry.'
+    },
+    {
+        id: 5,
+        name: 'Moda do Chef',
+        img: 'images/pizza5.png',
+        price: 29.90,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Uma receita especial do chef, com ingredientes selecionados e um sabor único.'
+    },
+    {
+        id: 6,
+        name: 'Moda da Casa',
+        img: 'images/pizza6.png',
+        price: 26.50,
+        sizes: ['320g', '530g', '860g'],
+        description: 'A combinação perfeita de queijos, presunto, tomate seco e rúcula.'
+    },
+    {
+        id: 7,
+        name: 'Chocolate',
+        img: 'images/pizza7.png',
+        price: 30.90,
+        sizes: ['320g', '530g', '860g'],
+        description: 'Uma deliciosa pizza doce com creme de chocolate, finalizada com pedaços de chocolate ao leite.'
+    }
 ];
